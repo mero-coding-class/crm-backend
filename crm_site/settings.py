f48f2c43278d3366b9de0a5c9838d0559cb44751
Mero Coding Class <merocodingclass@gmail.com>
2025-08-08 11:01:08 +0545

    Update settings.py

--- crm_site/settings.py
+++ crm_site/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://crm-backend-e8uw.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','crm-backend-e8uw.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
